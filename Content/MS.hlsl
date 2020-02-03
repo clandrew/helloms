930e369575ae25661505e879514e097ad9824500
Claire Andrews <clandrew@microsoft.com>
2020-02-03 11:12:20 -0800

    Split up the work into 2 threads

--- Content/MS.hlsl
+++ Content/MS.hlsl
@@ -21,7 +21,7 @@ float4 TransformPosition(float4 v)
 }
 
 [outputtopology("triangle")]
-[numthreads(1, 1, 1)]
+[numthreads(2, 1, 1)]
 void MSMain(
 	in uint3 groupID : SV_GroupID,
 	in uint3 threadInGroup : SV_GroupThreadID,
@@ -31,7 +31,7 @@ void MSMain(
 	const uint numVertices = 8;
 	const uint numPrimitives = 12;
 	SetMeshOutputCounts(numVertices, numPrimitives);
-	
+
 	verts[0].pos = TransformPosition(float4(-0.5f, -0.5f, -0.5f, 1.0f));
 	verts[0].color = float3(0.0f, 0.0f, 0.0f);
 
@@ -55,17 +55,27 @@ void MSMain(
 
 	verts[7].pos = TransformPosition(float4(0.5f, 0.5f, 0.5f, 1.0f));
 	verts[7].color = float3(1.0f, 1.0f, 1.0f);
-	
-	idx[0] = uint3(0, 2, 1);
-	idx[1] = uint3(1, 2, 3);
-	idx[2] = uint3(4, 5, 6);
-	idx[3] = uint3(5, 7, 6);
-	idx[4] = uint3(0, 1, 5);
-	idx[5] = uint3(0, 5, 4);
-	idx[6] = uint3(2, 6, 7);
-	idx[7] = uint3(2, 7, 3);
-	idx[8] = uint3(0, 4, 6);
-	idx[9] = uint3(0, 6, 2);
-	idx[10] = uint3(1, 3, 7);
-	idx[11] = uint3(1, 7, 5);
+
+	// Although we're running on multiple threads, the threads share
+	// the same output arrays (e.g., of indices).
+	if (threadInGroup.x == 0)
+	{
+
+		idx[0] = uint3(0, 2, 1);
+		idx[1] = uint3(1, 2, 3);
+		idx[2] = uint3(4, 5, 6);
+		idx[3] = uint3(5, 7, 6);
+		idx[4] = uint3(0, 1, 5);
+		idx[5] = uint3(0, 5, 4);
+	}	
+	else
+	{
+
+		idx[6] = uint3(2, 6, 7);
+		idx[7] = uint3(2, 7, 3);
+		idx[8] = uint3(0, 4, 6);
+		idx[9] = uint3(0, 6, 2);
+		idx[10] = uint3(1, 3, 7);
+		idx[11] = uint3(1, 7, 5);
+	}	
 }
